a77719f165606ed9313249e54acbb0a3134f7372
Lawani-EJ <elyonjohn60@gmail.com>
2024-07-31 14:10:05 +0100

    Modified the calculator application using fucntions to enhance performance Began by defining the calculator function with paramaters num1 and num2 Then i prompted the user to enter the operator And began by adding basic logic from the previous logic performed Handled some errors like whenever the user tries to divide a number by 0 it gets an error and handling when the users enters a wrong operator.

--- Calculator-app/script.py
+++ Calculator-app/script.py
@@ -1,19 +1,42 @@
-#  To create a calculator app that can perform basic arithmetic operations.
-operation = input("Enter Your Operator: ")
-number_1 = int(input("Enter Your first number: "))
-number_2 = int(input("Enter Your second number: "))
+# #  To create a calculator app that can perform basic arithmetic operations.
+# operation = input("Enter Your Operator: ")
+# number_1 = int(input("Enter Your first number: "))
+# number_2 = int(input("Enter Your second number: "))
 
-if operation == "+":
-    result = number_1+number_2
-    print(result)
-elif operation == "-":
-    result = number_1 - number_2
-    print(result)
-elif operation == "/":
-    result = number_1 / number_2
-    print(result)
-elif operation == "*":
-    result = number_1*number_2
-    print(result)
-else:
-    print(f"{operation} This value you have entered is not a valid operator please try again.")
+# if operation == "+":
+#     result = number_1+number_2
+#     print(result)
+# elif operation == "-":
+#     result = number_1 - number_2
+#     print(result)
+# elif operation == "/":
+#     result = number_1 / number_2
+#     print(result)
+# elif operation == "*":
+#     result = number_1*number_2
+#     print(result)
+# else:
+#     print(f"{operation} This value you have entered is not a valid operator please try again.")
+
+def calculator(num1,num2):
+        operation = input("Enter Your Operator: ")
+        if operation == "+":
+            result = num1+num2
+        elif operation == "-":
+            result = num1 - num2
+        elif operation == "/":
+            if num2 == 0:
+                print("Error: O division is not allowed")
+            else:
+                result = num1 / num2
+        elif operation == "*":
+            result = num1*num2
+        else:
+                print(f"{operation} This value you have entered is not a valid operator please try again.")
+        return result
+
+num1 = int(input("Enter Your first number: "))
+num2 = int(input("Enter Your second number: "))
+
+result = calculator(num1,num2)
+print(result)
