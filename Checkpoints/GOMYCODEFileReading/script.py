4761ca9073e0b94c972d4daf4c7584c970062ee3
Lawani-EJ <elyonjohn60@gmail.com>
2024-09-16 23:02:43 +0100

    Updated and modified the solutions to FileReading checkpoints.

--- Checkpoints/GOMYCODEFileReading/script.py
+++ Checkpoints/GOMYCODEFileReading/script.py
@@ -1,13 +1,13 @@
 # Read an Entire Text File 
-def read_enitre_file(file_path):
+def read_entire_file(file_path):
     try:
         with open(file_path, 'r') as file:
             content = file.read()
             return content
     except FileNotFoundError:
-        return "FIle not Found."
+        return "File not Found."
     
-print(read_enitre_file("Example3.txt"))
+print(read_entire_file("Example3.txt"))
 
 
 # Read the first n lines of a file 
@@ -19,7 +19,8 @@ def read_first_n_lines(file_path, n):
     except FileNotFoundError:
         return "File not found."
 
-print(read_enitre_file("Example3.txt"))
+print(read_first_n_lines("Example3.txt", 1))
+
 
 # Read the last n lines of a file 
 from collections import deque
@@ -32,7 +33,7 @@ def read_last_n_lines(file_path, n):
     except FileNotFoundError:
         return "File not found."
     
-print(read_enitre_file("Example3.txt"))
+print(read_last_n_lines("Example3.txt", 1))
 
 # Count the number of words in a file 
 def count_words_in_file(file_path):
@@ -43,13 +44,12 @@ def count_words_in_file(file_path):
         return len(words)
     except FileNotFoundError:
         return "File not found."
-
-# Example usage:
+print(count_words_in_file("Example3.txt"))
 file_path = 'Example3.txt'
-print(f"Number of words: {count_words_in_file(Example3.txt)}")
+print(f"The Number of words Present: {count_words_in_file(file_path)}")
 
 
-# (Bonus) Read the last n lines of a file (Alternative method) 
+# (Bonus) Read the last n lines of a file (Alternative method)  
 def read_last_n_lines_alt(file_path, n):
     try:
         with open(file_path, 'r') as file:
@@ -57,8 +57,7 @@ def read_last_n_lines_alt(file_path, n):
         return ''.join(lines[-n:])
     except FileNotFoundError:
         return "File not found."
-
 # Example usage:
 file_path = 'Example3.txt'
 n = 5
-print(read_last_n_lines_alt(Example3.txt, n))
+print(read_last_n_lines_alt(file_path, n))
